2d4c2597353c4a6cf49a605ce3fa00db1d072f85
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-25 16:20:10 +0000

    import bootstrap.min css into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
