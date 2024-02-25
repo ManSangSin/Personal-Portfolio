4cd8dcb6659c43e3b03075189b6b246781d4edd8
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-02-25 15:48:29 +0000

    imported components into App.jsx
    About, Footer, Header, Home placeholder components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import About from "./components/about/About";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
