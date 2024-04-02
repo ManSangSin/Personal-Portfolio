22f19ce51d1dc9af6acba04fce00fe57a2fa362d
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-04-02 21:57:32 +0100

    Create Nav component and import into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Nav from "./components/nav/Nav";
 
 function App() {
   return (
     <>
       <Header />
+      <Nav />
       <Home />
       <About />
       <Footer />
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <>
+      <div>I am Nav</div>
+    </>
+  );
+};
+
+export default Nav;
