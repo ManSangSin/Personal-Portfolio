2d7a967d772fb8d693041de7740d287775b02108
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-04-03 01:41:46 +0100

    import components into App.jsx
    Imported: Contact, Faq, Info, Projects components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Faq from "./components/faq/Faq";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Info from "./components/info/Info";
 import Nav from "./components/nav/Nav";
+import Projects from "./components/projects/Projects";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Header />
       <Nav />
       <Home />
+      <Projects />
+      <Info />
+      <Contact />
+      <Faq />
       <About />
       <Footer />
     </>
