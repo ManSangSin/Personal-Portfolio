abbb7147bb5fea62aac3de8df869335be9c73e79
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-04-04 00:20:46 +0100

    create isWelcomeActive state and use setTimeout to hide welcome element

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,9 +1,23 @@
 import "./welcome.css";
 
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Welcome = () => {
+  const [isWelcomeActive, setWelcomeActive] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setWelcomeActive(false);
+    }, 2500),
+      [];
+  });
   return (
     <>
-      <div className="welcomeContainer">I am Welcome</div>
+      {isWelcomeActive ? (
+        <div className="welcomeContainer">I am Welcome</div>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
