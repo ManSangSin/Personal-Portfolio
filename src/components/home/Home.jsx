1a8a3788fe691cdb9546f0e8288bc30eb6146092
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-04-05 11:43:48 +0100

    migrate text from About to Home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>I am Home</div>;
+  return (
+    <>
+      <p>
+        I’m a passionate software developer with a love for elegant code and
+        creative problem-solving. My journey began at CodeYourFuture, where I
+        honed my skills as a full stack developer. From front-end magic to
+        back-end wizardry, I thrive on building robust applications that make an
+        impact.
+      </p>
+    </>
+  );
 };
 
 export default Home;
