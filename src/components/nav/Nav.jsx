47d691c4adba8195bea0fee7e1bb2673abdc28ea
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-04-05 08:52:08 +0100

    assign classes to ul and li elements

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,23 +1,25 @@
+import "./nav.css";
+
 const Nav = ({ setActiveSection }) => {
   function handleClick() {
     setActiveSection(event.target.id);
   }
   return (
     <>
-      <ul>
-        <li onClick={handleClick} id="Home">
+      <ul className="navUnorderedList">
+        <li onClick={handleClick} id="Home" className="navListItem">
           Home
         </li>
-        <li onClick={handleClick} id="Projects">
+        <li onClick={handleClick} id="Projects" className="navListItem">
           Projects
         </li>
-        <li onClick={handleClick} id="Info">
+        <li onClick={handleClick} id="Info" className="navListItem">
           Info
         </li>
-        <li onClick={handleClick} id="Contact">
+        <li onClick={handleClick} id="Contact" className="navListItem">
           Contact
         </li>
-        <li onClick={handleClick} id="Faq">
+        <li onClick={handleClick} id="Faq" className="navListItem">
           FAQ
         </li>
       </ul>
