c65168953de8c7c3b529a02973e7a9ea5ac4a56c
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2024-04-16 23:34:31 +0100

    create projectData array and map data in component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,7 +1,48 @@
 const Projects = () => {
+  const projectData = [
+    {
+      name: "Rhythm Code",
+      type: "Group Project",
+      date: {
+        month: "Oct",
+        year: 2023,
+      },
+      link: "https://starter-kit-al84.onrender.com/",
+    },
+    {
+      name: "Radio Project",
+      type: "Group Project",
+      date: {
+        month: "Jan",
+        year: 2024,
+      },
+      link: "https://stay-tuned-radio-frontend.onrender.com/",
+    },
+    {
+      name: "Project Name",
+      type: "Individual",
+      date: {
+        month: "Apr",
+        year: 2024,
+      },
+      link: "http://www.google.com",
+    },
+  ];
+
+  const projectCards = projectData.map((project) => {
+    return (
+      <div>
+        <a target="_blank" rel="noopener" href={project.link}>
+          {project.date.month}.{project.date.year} / {project.type} /{" "}
+          {project.name}
+        </a>
+      </div>
+    );
+  });
   return (
     <>
-      <div>I am Projects</div>
+      <div>Projects (Design & Develop)</div>
+      {projectCards}
     </>
   );
 };
